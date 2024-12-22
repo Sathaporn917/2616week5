b2425ef10a6fb7944a15abee92b77003a1ab4cae
SathapornXD <9arthurnova@gmail.com>
2024-12-22 18:13:55 +0700

    final2

---
+++ ex1.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en"data-bs-theme="dark"    >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ใช้งาน Bootstrap</title>
+</head>
+
+     <!-- Latest compiled and minified CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anuphan:wght@100..700&family=Charmonman:wght@400;700&family=Itim&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Mitr:wght@200;300;400;500;600;700&family=Pattaya&family=Sriracha&display=swap" rel="stylesheet">
+
+ <style>
+      body {
+       
+        font-family: "Charmonman", serif;
+        font-weight: 600;
+        font-style: 20px;
+        margin-left: 20px;
+        margin-top :   20px;
+        margin-bottom : 80px;
+      }
+
+
+ </style>
+
+<body>
+   <center><h1>ทดสอบการใช้งาน Bootstrap</h1></center>
+   พัฒนาโดย 653485013 นายอดิภา บัวทอง<br>
+   หมู่เรียน 26.16 <br>
+   <div class="alert alert-danger">
+  <strong>ยินดีด้วย!</strong> คุณถูกจับแล้ว
+</div>
+<br>
+<button type="button" class="btn btn-warning">you bitch</button>
+<button type="button" class="btn btn-primary">dumn as fuck</button>
+<br><br>
+<button type="button" class="btn btn-outline-warning">hell</button>
+<button type="button" class="btn btn-outline-danger">matherfucker</button>
+<br><br>
+<div class="card" style="width:500px">
+  <img class="card-img-top" src="travis-scott.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">I'll be that pretty matherfucker</h4>
+    <p class="card-text">You can't buy vision and you can't buy aesthetic</p>
+    <a href="https://www.youtube.com/watch?v=3OUdeW4HmgE" class="btn btn-primary">Go to hell bitch</a>
+  </div>
+</div>
+ <br><br>
+ กิจกรรมที่ชอบ :
+ <div class="dropdown">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
+    กรุณาเลือกกิจกรรมที่ชอบ
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="https://www.youtube.com/watch?v=ekr2nIex040">i </a></li>
+    <li><a class="dropdown-item" href="#https://www.youtube.com/watch?v=aAkMkVFwAoo">love </a></li>
+    <li><a class="dropdown-item" href="https://www.youtube.com/watch?v=98zHKN-xSHk">you </a></li>
+  </ul>
+</div>
+</body>
+   
+  <!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</html>
---
+++ ex2.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en"data-bs-theme="dark"    >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ใช้งาน Bootstrap</title>
+</head>
+
+     <!-- Latest compiled and minified CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anuphan:wght@100..700&family=Charmonman:wght@400;700&family=Itim&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Mitr:wght@200;300;400;500;600;700&family=Pattaya&family=Sriracha&display=swap" rel="stylesheet">
+
+ <style>
+      body {
+       
+        font-family: "Charmonman", serif;
+        font-weight: 600;
+        font-style: 20px;
+        margin-left: 200px;
+        margin-top :   20px;
+        margin-bottom : 80px;
+        margin-right :   200px;
+      }
+
+
+ </style>
+
+<body>
+   <center><h1>โปรแกรมบันทึกข้อมูล</h1></center>
+   
+   <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">ชื่อ Rapper</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">อายุ</label>
+    <input type="text" class="form-control" id="inputPassword4">
+  </div>
+  <div class="col-6">
+    <label for="inputAddress" class="form-label">ที่อยู่</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="col-6">
+    <label for="inputAddress2" class="form-label">แนวเพลงหลัก</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">รางวัลที่ได้รับ</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">ปีที่เริ่มเข้าวงการ</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">วันเกิด</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">ชื่อในการแสดงl</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  <div class="col-md-3">
+    <label for="inputState" class="form-label">สัญชาติ</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-3">
+    <label for="inputZip" class="form-label">awards</label>
+    <input type="text" class="form-control" id="inputZip">
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+    <button type="reset" class="btn btn-primary">dumn as fuck</button>
+  </div>
+</form>
+
+
+</body>
+   
+  <!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</html>
